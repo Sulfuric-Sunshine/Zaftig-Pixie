8c422fdfb1b78f6f4655931becc73eda2cb9195d
jessicalc <jessica@crayoncollective.com>
2015-10-09 00:18:09 -0700

    removed unnecessary logged_in property on user model
    Because passport takes care of that

--- client/models/UserModel.js
+++ client/models/UserModel.js
@@ -15,18 +15,13 @@ var UserModel = Backbone.Model.extend({
   },
 
   loginUser: function() {
-    debugger;
     console.log("-------");
     console.log("Logging in user");
     console.log("-------");
-    if (!this.get('logged_in')) {
-      window.location.href = '/auth/twitter';
-      this.set('logged_in', true);
-    }
+    window.location.href = '/auth/twitter';
   },
 
   logOut: function() {
-    this.set('logged_in', false)
     window.location.href = '/logout';
   }
 
